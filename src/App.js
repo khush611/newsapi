a54935688006af6577d2bc610593116262c3f2b0
Khusboo Kumari <khushboobhushan611@gmail.com>
2020-11-02 20:18:09 +0530

    added search bar

--- src/App.js
+++ src/App.js
@@ -1,50 +1,38 @@
-// import React from "react";
-// import DataFetcher from "./DataFetcher";
-
-// function App() {
-//   var url =
-//     "https://newsapi.org/v2/everything?q=bitcoin&apiKey=ed1eee6b688545cc91022ebf7ddade91";
-//   var req = new Request(url);
-//   //"https://swapi.dev/api/people/1/">
-//   //<p>{JSON.stringify(data)}</p>
-//   //data.map(d=>(<p>{d.title}</p>))
-//   return (
-//     <div>
-//       <DataFetcher url={req}>
-//         {({ data, loading }) =>{
-//           if(loading){
-//             <h1>Loading...</h1>
-//           }
-//           else {
-//            for(var i=0;i<data.length;i++){
-//            <p>{data[i].title}</p>
-//            }
-//           }
-//         }
-//       }
-//       </DataFetcher>
-//     </div>
-//   );
-// }
-
-// export default App;
 import React, {Component} from "react"
 import "./App.css"
-// https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/fetch
-// https://swapi.co/
-// https://medium.com/javascript-scene/master-the-javascript-interview-what-is-a-promise-27fc71e77261
-
 class App extends Component {
     constructor() {
         super()
         this.state = {
-            data: []
+            data: [],
+            searchItem:"",
+            searchStored:""
         }
+        this.handleChange=this.handleChange.bind(this)
+        this.addItem=this.addItem.bind(this)
+        this.loadUrl=this.loadUrl.bind(this)
     }
-    
-    componentDidMount() {
-        this.setState({loading: true})
-        fetch("https://newsapi.org/v2/everything?q=bitcoin&apiKey=ed1eee6b688545cc91022ebf7ddade91")
+    handleChange(event) {
+        this.setState({
+           searchItem: event.target.value
+            })
+            console.log(this.state.searchItem)
+    }
+    addItem(event){
+        event.preventDefault()
+        this.setState({
+            searchStored:this.state.searchItem
+        })
+       //  this.setState({
+       //      searchItem:""
+       //  })
+        console.log(this.state.searchStored)
+   }
+   
+    loadUrl(event) {
+        event.preventDefault()
+       if(this.state.todoList!=="" && this.state.todoList!==" "){
+        fetch(`https://newsapi.org/v2/everything?q=${this.state.searchStored}&apiKey=ed1eee6b688545cc91022ebf7ddade91`)
             .then(response => response.json())
             .then(data => {
                 this.setState({
@@ -52,11 +40,19 @@ class App extends Component {
                 })
                 console.log(this.state.data)
             })
+        } else {
+            console.log("nothing")
+        }
     }
     
     render() {
         return (
             <div>
+                  <form>
+                <input type="text" name="todo" value={this.state.searchItem} onChange={this.handleChange}/>
+                <button onClick={this.addItem}>Search</button>
+                <button onClick={this.loadUrl}>See</button>
+            </form>
                {this.state.data.map((item,index)=>{
                  return(
                    <div>
@@ -64,7 +60,7 @@ class App extends Component {
                    {item.title}
                  </h2>
                  <img src={item.urlToImage} alt="pic" className="image"/>
-                <a href={item.url} className="button"><button >Read More...</button></a>
+                <a href={item.url} className="button" target="_blank"><button >Read More...</button></a>
                  <hr/>
                  </div>
                  )
